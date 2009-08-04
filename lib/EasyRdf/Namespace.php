8db2237a5290fec971b6740544c7566194578215
Nicholas J Humfrey <njh@aelius.com>
2009-08-04 22:23:21 +0000

    Documented and tested the namespace class.

--- lib/EasyRdf/Namespace.php
+++ lib/EasyRdf/Namespace.php
@@ -1,22 +1,72 @@
 <?php
 
+/**
+  * A namespace registry and manipulation class.
+  */
 class EasyRdf_Namespace
 {
-    protected static $_namespaces = array(
+    private static $_namespaces = array(
       'dc' => 'http://purl.org/dc/elements/1.1/',
       'foaf' => 'http://xmlns.com/foaf/0.1/',
       'rdfs' => 'http://www.w3.org/2000/01/rdf-schema#',
       'xsd' => 'http://www.w3.org/2001/XMLSchema#'
     );
-    
 
+    /**
+      * Return a namespace given its prefix.
+      *
+      * @param string $short The namespace prefix (eg 'foaf')
+      * @return string The namespace URI (eg 'http://xmlns.com/foaf/0.1/')
+      */
+    public static function get($short)
+    {
+        return self::$_namespaces[$short];
+    }
+
+    /**
+      * Register a new namespace.
+      *
+      * @param string $short The namespace prefix (eg 'foaf')
+      * @param string $long The namespace URI (eg 'http://xmlns.com/foaf/0.1/')
+      */
     public static function add($short, $long)
     {
         self::$_namespaces[$short] = $long;
     }
-    
-    public static function get($short)
+
+    /**
+      * Shorten a URI by substituting in the namespace prefix.
+      *
+      * @param string $uri The full URI (eg 'http://xmlns.com/foaf/0.1/name')
+      * @return string The shortened URI (eg 'foaf_name')
+      */
+    public static function shorten($uri)
     {
-        return self::$_namespaces[$short];
+        foreach (self::$_namespaces as $short => $long) {
+            if (strpos($uri, $long) === 0) {
+                return $short . '_' . substr($uri, strlen($long));
+            }
+        }
+        return NULL;
+    }
+
+    /**
+      * Expand a shorterned URI back into a full URI.
+      *
+      * @param string $short_uri The short URI (eg 'foaf_name')
+      * @return string The full URI (eg 'http://xmlns.com/foaf/0.1/name')
+      */
+    public static function expand($short_uri)
+    {
+        if (preg_match("/^(\w+?)_(.+)$/", $short_uri, $matches)) {
+            $long = self::get($matches[1]);
+            if ($long) {
+                return $long . $matches[2];
+            } else {
+                return NULL;
+            }
+        } else {
+            return NULL;
+        }
     }
 }
---
+++ test/EasyRdf/NamespaceTest.php
@@ -0,0 +1,57 @@
+<?php
+require_once 'PHPUnit/Framework.php';
+require_once 'EasyRdf/Namespace.php';
+
+class EasyRdf_NamespaceTest extends PHPUnit_Framework_TestCase
+{
+    public function testGetDcNamespace()
+    {
+        $this->assertEquals('http://purl.org/dc/elements/1.1/', EasyRdf_Namespace::get('dc'));
+    }
+
+    public function testGetFoafNamespace()
+    {
+        $this->assertEquals('http://xmlns.com/foaf/0.1/', EasyRdf_Namespace::get('foaf'));
+    }
+
+    public function testGetRdfsNamespace()
+    {
+        $this->assertEquals('http://www.w3.org/2000/01/rdf-schema#', EasyRdf_Namespace::get('rdfs'));
+    }
+
+    public function testGetXsdNamespace()
+    {
+        $this->assertEquals('http://www.w3.org/2001/XMLSchema#', EasyRdf_Namespace::get('xsd'));
+    }
+
+    public function testAddNamespace()
+    {
+        EasyRdf_Namespace::add('po', 'http://purl.org/ontology/po/');
+        $this->assertEquals('http://purl.org/ontology/po/', EasyRdf_Namespace::get('po'));
+    }
+
+    public function testShortenFoafName()
+    {
+        $this->assertEquals('foaf_name', EasyRdf_Namespace::shorten('http://xmlns.com/foaf/0.1/name'));
+    }
+
+    public function testShortenUnknownUrl()
+    {
+        $this->assertEquals(NULL, EasyRdf_Namespace::shorten('http://www.aelius.com/njh/'));
+    }
+
+    public function testExpandFoafName()
+    {
+        $this->assertEquals('http://xmlns.com/foaf/0.1/name', EasyRdf_Namespace::expand('foaf_name'));
+    }
+
+    public function testExpandMissingUnderscore()
+    {
+        $this->assertEquals(NULL, EasyRdf_Namespace::expand('unknown'));
+    }
+
+    public function testExpandUnknown()
+    {
+        $this->assertEquals(NULL, EasyRdf_Namespace::expand('unknown_unknown'));
+    }
+}
