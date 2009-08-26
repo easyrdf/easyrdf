bfae0ee8254a193d7f5922f357f1f29474bab6aa
Nicholas J Humfrey <njh@aelius.com>
2009-08-26 20:32:03 +0000

    Added the XHTML namespace

--- lib/EasyRdf/Namespace.php
+++ lib/EasyRdf/Namespace.php
@@ -11,6 +11,7 @@ class EasyRdf_Namespace
       'owl' => 'http://www.w3.org/2002/07/owl#',
       'rdf' => 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
       'rdfs' => 'http://www.w3.org/2000/01/rdf-schema#',
+      'xhtml' => 'http://www.w3.org/1999/xhtml/vocab#',
       'xsd' => 'http://www.w3.org/2001/XMLSchema#'
     );
 
