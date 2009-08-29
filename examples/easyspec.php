65fa799dc378cae1073c23467ec5ad2e6f01f990
Nicholas J Humfrey <njh@aelius.com>
2009-08-29 20:54:31 +0000

    Improvements to property display. - support for properties in domain of OWL:Thing - suport for properties of type rdf_Property, owl_Property, owl_ObjectProperty, owl_DatatypeProperty

--- examples/easyspec.php
+++ examples/easyspec.php
@@ -2,6 +2,11 @@
     set_include_path(get_include_path() . PATH_SEPARATOR . '../lib/');
     require_once "EasyRdf/Graph.php";
     $url = $_GET['url'];
+    
+    # TODO LIST:
+    # - display rdfs_range
+    # - make use of rdfs_isDefinedBy?
+    # - do clever things witgh rdfs_subPropertyOf?
 ?>
 <html>
 <head><title>EasyRdf Spec Maker</title></head>
@@ -14,6 +19,7 @@
 <?php
     if ($url) {
         $graph = new EasyRdf_Graph( $url );
+        $owl_thing = $graph->getResource('http://www.w3.org/2002/07/owl#Thing', 'owl_Class');
         $ontology = $graph->firstOfType('owl_Ontology');
     }
     
@@ -25,23 +31,33 @@
     function shorten($ns,$uri) {
         if (is_object($ns)) $ns = $ns->getUri();
         if (is_object($uri)) $uri = $uri->getUri();
+        if ($uri == 'http://www.w3.org/2002/07/owl#Thing') return 'Owl_Thing';
         if (strpos($uri, $ns) === 0) {
             return substr($uri, strlen($ns));
         } else {
             return null;
         }
     }
-    
-    function getClassProperties($graph, $ontology, $class) {
+
+    function getAllProperties($graph, $ontology)
+    {
+        $property_types = array('rdf_Property','owl_Property','owl_ObjectProperty','owl_DatatypeProperty');
         $properties = array();
-        foreach ($graph->allOfType('owl_ObjectProperty') as $property) {
-            if (in_array($class, $property->all('rdfs_domain'))) {
-                $short = shorten($ontology, $property);
-                if ($short) {
-                    array_push($properties, "$short - <i>".$property->join('rdfs_comment')."</i>");
-                } else {
-                    array_push($properties, link_to($property));
-                }
+        foreach ($property_types as $property_type) {
+            foreach ($graph->allOfType($property_type) as $property) {
+                $name = shorten($ontology, $property);
+                $properties[$name] = $property;
+            }
+        }
+        return $properties;
+    }
+    
+    function getClassProperties($class, $all_properties) {
+        $properties = array();
+        foreach ($all_properties as $name => $property) {
+            if (in_array($class, $property->all('rdfs_domain')))
+            {
+                array_push($properties, "$name - <i>".$property->join('rdfs_comment')."</i>");
             }
         }
         return $properties;
@@ -59,8 +75,10 @@
         #if ($ontology->dc_contributor)  # FIXME: implement this
         echo "</dl>\n";
         foreach ($ontology->all('rdfs_comment') as $comment) { echo "<p>$comment</p>\n"; }
+        foreach ($ontology->all('dc_description') as $description) { echo "<p>$description</p>\n"; }
         
         echo "<h2>Classes</h2>\n";
+        $all_properties = getAllProperties($graph, $ontology);
         foreach ($graph->allOfType('owl_Class') as $class) {
             $class_name = shorten($ontology,$class);
             if ($class_name == null) continue;
@@ -68,7 +86,12 @@
             echo "<h3>$class_name</h3>\n";
             foreach ($class->all('rdfs_comment') as $comment) { echo "<p>$comment</p>\n"; }
             echo "<dl>\n";
-            if ($class->rdfs_subClassOf) {
+            
+            if ($class != $owl_thing) {
+                # Make class a subclass of owl_Thing if it isn't a subclass of anything else
+                if (count($class->all('rdfs_subClassOf')) == 0) {
+                    $class->set('rdfs_subClassOf', $owl_thing);
+                }
                 echo "<dt>SubClass of:</dt>\n";
                 foreach ($class->all('rdfs_subClassOf') as $subClass) {
                     $short = shorten($ontology,$subClass);
@@ -90,10 +113,10 @@
                     }
                 }
             }
-            $properties = getClassProperties( $graph, $ontology, $class );
+            $properties = getClassProperties( $class, $all_properties );
             if ($properties) {
-              echo "<dt>Properties:</dt>\n";
-              foreach ($properties as $property) { echo "<dd>$property</d>\n"; }
+                echo "<dt>Properties:</dt>\n";
+                foreach ($properties as $property) { echo "<dd>$property</d>\n"; }
             }
             echo "</dl>\n";
             echo "</div>";
