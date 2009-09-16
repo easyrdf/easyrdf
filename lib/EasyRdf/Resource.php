d375a8c463dfd248ff8b19ed437ac1979589237a
Nicholas J Humfrey <njh@aelius.com>
2009-09-16 22:16:47 +0000

    Code style changes to comply with Zend

--- lib/EasyRdf/Resource.php
+++ lib/EasyRdf/Resource.php
@@ -5,16 +5,15 @@ require_once "EasyRdf/Namespace.php";
 class EasyRdf_Resource
 {
     /** The URI for this resource */
-    private $uri = null;
+    private $_uri = null;
     
     /** Associative array of properties */
-    private $properties = array();
+    private $_properties = array();
     
     /** Enable / disable PHP's magic __call() method */
-    private static $magicEnabled = true;
+    private static $_magicEnabled = true;
 
 
-    
     public static function disableMagic()
     {
         self::$magicEnabled = false;
@@ -28,37 +27,39 @@ class EasyRdf_Resource
     // This shouldn't be called directly
     public function __construct($uri)
     {
-        $this->uri = $uri;
+        $this->_uri = $uri;
     }
     
-    public function getUri() {
-        return $this->uri;
+    /** Returns the URI for the resource. */
+    public function getUri()
+    {
+        return $this->_uri;
     }
     
     public function set($property, $value)
     {
         if ($property == null or $value == null) {
             return null;
-        } else if (array_key_exists($property, $this->properties)) {
-            $values = $this->properties[$property];
+        } else if (array_key_exists($property, $this->_properties)) {
+            $values = $this->_properties[$property];
         } else {
             $values = array();
         }
-        # Add to array of values, if it isn't already there
+        // Add to array of values, if it isn't already there
         if (!in_array($value, $values)) {
             array_push($values, $value);
         }
-        return $this->properties[$property] = $values;
+        return $this->_properties[$property] = $values;
     }
 
     public function get($property)
     {
-        if (isset($this->properties[$property])) {
-            if (is_array($this->properties[$property])) {
-                $values = $this->properties[$property];
+        if (isset($this->_properties[$property])) {
+            if (is_array($this->_properties[$property])) {
+                $values = $this->_properties[$property];
                 return $values[0];
             } else {
-                return $this->properties[$property];
+                return $this->_properties[$property];
             }
         } else {
             return null;
@@ -67,11 +68,11 @@ class EasyRdf_Resource
     
     public function all($property)
     {
-        if (isset($this->properties[$property])) {
-            if (is_array($this->properties[$property])) {
-                return $this->properties[$property];
+        if (isset($this->_properties[$property])) {
+            if (is_array($this->_properties[$property])) {
+                return $this->_properties[$property];
             } else {
-                return array($this->properties[$property]);
+                return array($this->_properties[$property]);
             }
         } else {
             return array();
@@ -80,11 +81,12 @@ class EasyRdf_Resource
     
     public function join($property, $glue=' ')
     {
-        return join( $glue, $this->all($property) );
+        return join($glue, $this->all($property));
     }
     
-    public function isBnode() {
-        if (substr($this->uri, 0, 2) == '_:') {
+    public function isBnode()
+    {
+        if (substr($this->_uri, 0, 2) == '_:') {
             return true;
         } else {
             return false;
@@ -106,12 +108,12 @@ class EasyRdf_Resource
     # Return the namepace that this resource is part of
     public function ns()
     {
-        return EasyRdf_Namespace::namespaceOfUri($this->uri);
+        return EasyRdf_Namespace::namespaceOfUri($this->_uri);
     }
     
     public function shorten()
     {
-        return EasyRdf_Namespace::shorten($this->uri);
+        return EasyRdf_Namespace::shorten($this->_uri);
     }
     
     public function label()
@@ -123,7 +125,7 @@ class EasyRdf_Resource
         } else if ($this->get('dc_title')) {
             return $this->get('dc_title');
         } else {
-            return EasyRdf_Namespace::shorten($this->uri); 
+            return EasyRdf_Namespace::shorten($this->_uri); 
         }
     }
     
@@ -133,13 +135,11 @@ class EasyRdf_Resource
         echo '<pre>';
         echo '<b>'.$this->getUri()."</b>\n";
         echo 'Class: '.get_class($this)."\n";
-        echo 'Types: '.implode(', ',$this->types())."\n";
+        echo 'Types: '.implode(', ', $this->types())."\n";
         echo "Properties:</i>\n";
-        foreach ($this->properties as $property => $values)
-        {
+        foreach ($this->_properties as $property => $values) {
             echo "  $property => \n";
-            foreach ($values as $value)
-            {
+            foreach ($values as $value) {
                 echo "    $value\n";
             }
         }
@@ -149,28 +149,28 @@ class EasyRdf_Resource
     
     public function __call($name, $arguments)
     {
-        $method = substr($name,0,3);
-        $property = strtolower(substr($name,3,1)) . substr($name,4);
+        $method = substr($name, 0, 3);
+        $property = strtolower(substr($name, 3, 1)) . substr($name, 4);
         
         switch ($method) {
           case 'get':
               return $this->get($property);
-          break;
+              break;
           
           case 'all':
               return $this->all($property);
-          break;
+              break;
         
           default:
-             # FIXME: throw exception
-             return null;
-          break;
+              # FIXME: throw exception
+              return null;
+              break;
         }
     }
     
     public function __toString()
     {
-        return $this->uri;
+        return $this->_uri;
     }
 }
 
