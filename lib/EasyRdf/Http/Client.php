d1e7b829dd91da6a5bf139a914cb3ae4ce310b8b
Nicholas J Humfrey <njh@aelius.com>
2009-09-27 22:03:43 +0000

    Added Zend to credits for EasyRdf_Http_Client

--- lib/EasyRdf/Http/Client.php
+++ lib/EasyRdf/Http/Client.php
@@ -6,6 +6,7 @@
  * LICENSE
  *
  * Copyright (c) 2009 Nicholas J Humfrey.  All rights reserved.
+ * Copyright (c) 2005-2009 Zend Technologies USA Inc.
  *
  * Redistribution and use in source and binary forms, with or without 
  * modification, are permitted provided that the following conditions are met:
@@ -32,6 +33,7 @@
  *
  * @package    EasyRdf
  * @copyright  Copyright (c) 2009 Nicholas J Humfrey
+ *             Copyright (c) 2005-2009 Zend Technologies USA Inc.
  * @license    http://www.opensource.org/licenses/bsd-license.php
  * @version    $Id$
  */
