c45c81c832d6322a4c225e9ef4b7d67488a02a80
Nicholas J Humfrey <njh@aelius.com>
2009-11-23 00:13:05 +0000

    Re-wrote wiki docs script to get the documentation from the source files, rather than the HTML output of phpDocumentor

--- build/phpDocWiki.php
+++ build/phpDocWiki.php
@@ -1,83 +1,113 @@
 #!/usr/bin/env php
 <?php
 
-/* Tool to convert PhpDocumentor HTML output into wiki text.
-   This script is wrong but it was quick to write and works.
+/*
+   Tool to convert PHPDoc for classes into Wiki text.
 
    TODO:
-    - rewrite as PhpDocumenter template
+    - rewrite as PhpDocumenter/Doxygen output template
     - add parameter/return value documentation
     - add documentation for constants
     - add links to subversion line numbers
 */
 
-$html_doc_dir = dirname(__FILE__). "/../docs/EasyRdf";
+$source_dir = dirname(__FILE__). "/../lib/EasyRdf";
 $wiki_doc_dir = dirname(__FILE__). "/../../../wiki";
-if (!file_exists($html_doc_dir)) {
-    throw new Exception("HTML Documentation directory does not exist: $html_doc_dir");
+if (!file_exists($source_dir)) {
+    throw new Exception("Source directory does not exist: $source_dir");
 }
 if (!file_exists($wiki_doc_dir)) {
     throw new Exception("Wiki output directory does not exist: $wiki_doc_dir");
 }
 
-process_classes($html_doc_dir);
+process_directory($source_dir);
 
 
-
-function process_method($html) {
-    $output = '';
-
-    if (preg_match("/<h3>(static |)method (.+) <span/", $html, $matches)) {
-        $output .= "== ".$matches[2]."() ==\n\n";
-        print " - ".$matches[2]."()\n";
+function output_methods($wiki, $methods)
+{
+    ksort($methods);
+    foreach($methods as $name => $method) {
+        fwrite($wiki, "== $name() ==\n\n");
+        fwrite($wiki, "{{{".$method['signature']."}}}\n\n");
+        fwrite($wiki, $method['docs']."\n\n");
     }
-
-    if (preg_match("/<code>\s*(.+)\s*<\/code>/s", $html, $matches)) {
-        $code = str_replace('( ', '(', preg_replace("/([\r\n]+)/", ' ', $matches[1]));
-        $output .= "{{{".strip_tags($code)."}}}\n\n";
-    }
-    
-    $lines  = preg_split("/[\r\n]+/", $html);
-    foreach ($lines as $line) {
-        if (preg_match("/^\s+\w+/", $line)) {
-            $line = strip_tags(preg_replace("/<br \/>|<\/p>/", "\n", $line));
-            $output .= trim($line)."\n\n";
-        }
-    
-    }
-    
-    return $output;
 }
 
-function process_class($html_filepath, $wiki_filepath) {
-    print "Processing: $html_filepath\n";
-    print "Output: $wiki_filepath\n";
-    
-    $wiki = fopen($wiki_filepath, 'w');
-
-    $sections  = preg_split("/<hr \/>/", file_get_contents($html_filepath));
-    foreach ($sections as $section) {
-        if (preg_match("/<div class=\"function\">/", $section)) {
-            fwrite($wiki, process_method($section));
-        } else if (preg_match("/<div class=\"description\">(.+?)<\/div>/", $section, $matches)) {
-            fwrite($wiki, "#summary ".$matches[1]."\n\n");
-            fwrite($wiki, "<wiki:toc max_depth='2' />\n\n");
-            fwrite($wiki, "= Methods =\n\n");
-         }
-    }
-    
-    print "\n";
-}
-
-function process_classes($dirpath) {
+function output_class($class_name, $class_docs, $class_methods, $member_methods)
+{
     global $wiki_doc_dir;
     
+    $wiki = fopen("$wiki_doc_dir/$class_name.wiki",'w');
+    if ($wiki) {
+        fwrite($wiki, "#summary $class_docs\n\n");
+        fwrite($wiki, "<wiki:toc max_depth='2' />\n\n");
+        if (count($class_methods)>0) {
+            fwrite($wiki, "= Class Methods =\n\n");
+            output_methods($wiki, $class_methods);
+        }
+        if (count($member_methods)>0) {
+            fwrite($wiki, "= Member Methods =\n\n");
+            output_methods($wiki, $member_methods);
+        }
+    }
+    fclose($wiki);
+}
+
+function process_file($filepath)
+{
+    print "Processing: $filepath\n";
+    
+    $state = 'OUTDOC';
+    $class_name = null;
+    $class_docs = '';
+    $class_methods = array();
+    $member_methods = array();
+
+    $lines = preg_split("/[\r\n]+/", file_get_contents($filepath));
+    while ($line = array_shift($lines)) {
+        if (preg_match("{^\s*/\*\*\s*(.*)\s*$}", $line, $matches)) {
+            $docs = $matches[1] ? $matches[1]."\n" : "";
+            $state = 'INDOC';
+        } else if ($state == 'INDOC' and preg_match("{^\s*\* ([^/].+)$}", $line, $matches)) {
+            $line = $matches[1];
+            if (preg_match("{^@}", $line)) {
+                # FIXME: insert tag handling code here
+            } else {
+                $docs .= "$line\n";
+            }
+        } else if (preg_match("{\*/}", $line)) {
+            $state = 'OUTDOC';
+        } else if (preg_match("{^\s*class (\w+)}", $line, $matches)) {
+            $class_name = $matches[1];
+            $class_docs = trim($docs);
+        } else if (preg_match("{public (static )?function (\w+)(.*)\s*$}", $line, $matches)) {
+            $func = array(
+                'signature' => $matches[1]."function ".$matches[2].$matches[3],
+                'name' => $matches[2],
+                'docs' => trim($docs)
+            );
+            if ($matches[1] == 'static ') {
+                $class_methods[$matches[2]] = $func;
+            } else {
+                $member_methods[$matches[2]] = $func;
+            }
+        }
+    }
+    
+    if ($class_name) {
+        output_class($class_name, $class_docs, $class_methods, $member_methods);
+    }
+}
+
+function process_directory($dirpath)
+{
     $dh = opendir($dirpath);
     while($filename = readdir($dh)) {
-        $html_filepath = "$dirpath/$filename";
-        if (is_file($html_filepath) and preg_match("/^(EasyRdf_\w+)/",$filename,$matches)) {
-            $wiki_filepath = "$wiki_doc_dir/".$matches[1].".wiki";
-            process_class($html_filepath, $wiki_filepath);
+        $filepath = realpath("$dirpath/$filename");
+        if (is_file($filepath) and preg_match("/\.php$/",$filename)) {
+            process_file($filepath);
+        } else if (is_dir($filepath) and substr($filename,0,1) != '.') {
+            process_directory($filepath);
         }
     }
     closedir($dh);
