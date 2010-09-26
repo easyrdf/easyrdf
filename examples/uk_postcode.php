51d62fd9364e3ff291ccfe558ae42df2a2848e37
Nicholas J Humfrey <njh@aelius.com>
2010-09-26 13:18:12 +0100

    Created an example that resolves UK postcodes to co-ordinates using uk-postcodes.com

---
+++ examples/uk_postcode.php
@@ -0,0 +1,60 @@
+<?php
+    set_include_path(get_include_path() . PATH_SEPARATOR . '../lib/');
+    require_once "EasyRdf.php";
+    require_once "html_tag_helpers.php";
+
+    ## FIXME: one day EasyRdf will parse RDF/XML natively
+    require_once "EasyRdf/Parser/Rapper.php";
+
+    EasyRdf_Namespace::set('postcode', 'http://www.uk-postcodes.com/postcode/');
+    EasyRdf_Namespace::set('sr', 'http://data.ordnancesurvey.co.uk/ontology/spatialrelations/');
+    EasyRdf_Namespace::set('eg', 'http://statistics.data.gov.uk/def/electoral-geography/');
+    EasyRdf_Namespace::set('ag', 'http://statistics.data.gov.uk/def/administrative-geography/');
+?>
+<html>
+<head>
+  <title>UK Postcode Resolver</title>
+  <style type="text/css" media="all">
+    #map
+    {
+    	border: 1px gray solid;
+    	float: right;
+    	margin: 0 0 20px 20px;
+    }
+    th { text-align: right }
+    td { padding: 5px; }
+  </style>
+</head>
+<body>
+<h1>UK Postcode Resolver</h1>
+
+<?= form_tag() ?>
+  <?= text_field_tag('postcode', 'W1A 1AA', array('size'=>10)) ?>
+  <?= submit_tag() ?>
+<?= form_end_tag() ?>
+
+<?php
+    if (isset($_REQUEST['postcode'])) {
+        $postcode = str_replace(' ', '', strtoupper($_REQUEST['postcode']));
+        $docuri = "http://www.uk-postcodes.com/postcode/$postcode.rdf";
+        $graph = new EasyRdf_Graph($docuri);
+        $res = $graph->resource("postcode:$postcode");
+
+        $ll = $res->get('geo:lat').','.$res->get('geo:long');
+        print "<iframe id='map' width='500' height='250' frameborder='0' scrolling='no' src='http://maps.google.com/maps?f=q&amp;sll=$ll&amp;output=embed'></iframe>";
+        print "<table id='facts'>\n";
+        print "<tr><th>Easting:</th><td>" . $res->get('sr:easting') . "</td></tr>\n";
+        print "<tr><th>Northing:</th><td>" . $res->get('sr:northing') . "</td></tr>\n";
+        print "<tr><th>Longitude:</th><td>" . $res->get('geo:long') . "</td></tr>\n";
+        print "<tr><th>Latitude:</th><td>" . $res->get('geo:lat') . "</td></tr>\n";
+        print "<tr><th>Local Authority:</th><td>" . $res->get('ag:localAuthority')->label() . "</td></tr>\n";
+        print "<tr><th>Electoral Ward:</th><td>" . $res->get('eg:ward')->label() . "</td></tr>\n";
+        print "</table>\n";
+        
+        print "<div style='clear: both'></div>\n";
+
+        print $graph->dump();
+    }
+?>
+</body>
+</html>
