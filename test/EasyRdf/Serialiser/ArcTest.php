ac102f5e35e2191e080b1e59bdfa61058e374aa5
Nicholas J Humfrey <njh@aelius.com>
2010-10-07 18:29:50 +0100

    Restored ARC parser and serialiser tests, now that ARC2 works in E_STRICT mode.

---
+++ test/EasyRdf/Serialiser/ArcTest.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * EasyRdf
+ *
+ * LICENSE
+ *
+ * Copyright (c) 2009-2010 Nicholas J Humfrey.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. The name of the author 'Nicholas J Humfrey" may be used to endorse or
+ *    promote products derived from this software without specific prior
+ *    written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    EasyRdf
+ * @copyright  Copyright (c) 2009-2010 Nicholas J Humfrey
+ * @license    http://www.opensource.org/licenses/bsd-license.php
+ * @version    $Id$
+ */
+
+require_once dirname(dirname(dirname(__FILE__))).
+             DIRECTORY_SEPARATOR.'TestHelper.php';
+
+require_once 'EasyRdf/Serialiser/Arc.php';
+
+class EasyRdf_Serialiser_ArcTest extends EasyRdf_TestCase
+{
+    public function setUp()
+    {
+        if (requireExists('arc/ARC2.php')) {
+            $this->_graph = new EasyRdf_Graph();
+            $this->_serialiser = new EasyRdf_Serialiser_Arc();
+            parent::setUp();
+        } else {
+            $this->markTestSkipped(
+                "ARC2 library is not available."
+            );
+        }
+    }
+
+    function testSerialiseRdfXml()
+    {
+        $joe = $this->_graph->resource('http://www.example.com/joe#me');
+        $joe->set('foaf:name', 'Joe Bloggs');
+        $this->_graph->add(
+            $joe, 'foaf:project',
+            array('foaf:name' => 'Project Name')
+        );
+
+        $rdfxml = $this->_serialiser->serialise($this->_graph, 'rdfxml');
+        $this->assertNotNull($rdfxml);
+        $this->assertContains(
+            '<rdf:Description rdf:about="http://www.example.com/joe#me">',
+            $rdfxml
+        );
+        $this->assertContains(':name>Project Name<', $rdfxml);
+    }
+
+    function testSerialiseUnsupportedFormat()
+    {
+        $this->setExpectedException('EasyRdf_Exception');
+        $rdf = $this->_serialiser->serialise(
+            $this->_graph, 'unsupportedformat'
+        );
+    }
+}
