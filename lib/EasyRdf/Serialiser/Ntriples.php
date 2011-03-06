bc183bc5828de95e1b53aa4c2d85648b136ae7e9
Nicholas J Humfrey <njh@aelius.com>
2011-03-06 22:31:10 +0000

    Improved character encoding the the N-Triples serialiser.

--- lib/EasyRdf/Serialiser/Ntriples.php
+++ lib/EasyRdf/Serialiser/Ntriples.php
@@ -46,15 +46,18 @@
  */
 class EasyRdf_Serialiser_Ntriples extends EasyRdf_Serialiser
 {
+    private $_escChars = array();   // Character encoding cache
+
     /**
      * @ignore
      */
     protected function ntriplesResource($res)
     {
+        $id = $this->escape($res->getURI());
         if ($res->isBNode()) {
-            return $res->getURI();
+            return $id;
         } else {
-            return "<".$res->getURI().">";
+            return "<$id>";
         }
     }
 
@@ -66,19 +69,12 @@ class EasyRdf_Serialiser_Ntriples extends EasyRdf_Serialiser
         if ($obj instanceof EasyRdf_Resource) {
             return $this->ntriplesResource($obj);
         } else if ($obj instanceof EasyRdf_Literal) {
-            // FIXME: peform encoding of Unicode characters as described here:
-            // http://www.w3.org/TR/rdf-testcases/#ntrip_strings
-            $value = $obj->getValue();
-            $value = str_replace('\\', '\\\\', $value);
-            $value = str_replace('"', '\\"', $value);
-            $value = str_replace('\n', '\\n', $value);
-            $value = str_replace('\r', '\\r', $value);
-            $value = str_replace('\t', '\\t', $value);
-
+            $value = $this->escape($obj->getValue());
             if ($obj->getLang()) {
-                return '"' . $value . '"' . '@' . $obj->getLang();
+                $lang = $this->escape($obj->getLang());
+                return '"' . $value . '"' . '@' . $lang;
             } else if ($obj->getDatatypeUri()) {
-                $datatype = $obj->getDatatypeUri();
+                $datatype = $this->escape($obj->getDatatypeUri());
                 return '"' . $value . '"' . "^^<$datatype>";
             } else {
                 return '"' . $value . '"';
@@ -88,6 +84,80 @@ class EasyRdf_Serialiser_Ntriples extends EasyRdf_Serialiser
                 "Unable to serialise object to ntriples: ".gettype($obj)
             );
         }
+    }
+
+    /**
+     * @ignore
+     */
+    protected function escape($str)
+    {
+        if (strpos(utf8_decode(str_replace('?', '', $str)), '?') === false) {
+            $str = utf8_decode($str);
+        }
+
+        $result = '';
+        $strLen = strlen($str);
+        for ($i = 0; $i < $strLen; $i++) {
+            $c = $str[$i];
+            if (!isset($this->_escChars[$c])) {
+                $this->_escChars[$c] = $this->escapedChar($c, $this->unicodeCharNo($c));
+            }
+            $result .= $this->_escChars[$c];
+        }
+        return $result;
+    }
+
+    /**
+     * @ignore
+     */
+    protected function unicodeCharNo($c)
+    {
+        $cUtf = utf8_encode($c);
+        $bl = strlen($cUtf); /* binary length */
+        $r = 0;
+        switch ($bl) {
+            case 1: /* 0####### (0-127) */
+                $r = ord($cUtf);
+                break;
+            case 2: /* 110##### 10###### = 192+x 128+x */
+                $r = ((ord($cUtf[0]) - 192) * 64) +
+                     (ord($cUtf[1]) - 128);
+                break;
+            case 3: /* 1110#### 10###### 10###### = 224+x 128+x 128+x */
+                $r = ((ord($cUtf[0]) - 224) * 4096) +
+                     ((ord($cUtf[1]) - 128) * 64) +
+                     (ord($cUtf[2]) - 128);
+                break;
+            case 4: /* 1111#### 10###### 10###### 10###### = 240+x 128+x 128+x 128+x */
+                $r = ((ord($cUtf[0]) - 240) * 262144) +
+                     ((ord($cUtf[1]) - 128) * 4096) +
+                     ((ord($cUtf[2]) - 128) * 64) +
+                     (ord($cUtf[3]) - 128);
+                break;
+        }
+        return $r;
+    }
+
+    /**
+     * @ignore
+     */
+    protected function escapedChar($c, $no)
+    {
+        /* see http://www.w3.org/TR/rdf-testcases/#ntrip_strings */
+        if ($no < 9)        return "\\u" . sprintf('%04X', $no);  /* #x0-#x8 (0-8) */
+        if ($no == 9)       return '\t';                          /* #x9 (9) */
+        if ($no == 10)      return '\n';                          /* #xA (10) */
+        if ($no < 13)       return "\\u" . sprintf('%04X', $no);  /* #xB-#xC (11-12) */
+        if ($no == 13)      return '\r';                          /* #xD (13) */
+        if ($no < 32)       return "\\u" . sprintf('%04X', $no);  /* #xE-#x1F (14-31) */
+        if ($no < 34)       return $c;                            /* #x20-#x21 (32-33) */
+        if ($no == 34)      return '\"';                          /* #x22 (34) */
+        if ($no < 92)       return $c;                            /* #x23-#x5B (35-91) */
+        if ($no == 92)      return '\\';                          /* #x5C (92) */
+        if ($no < 127)      return $c;                            /* #x5D-#x7E (93-126) */
+        if ($no < 65536)    return "\\u" . sprintf('%04X', $no);  /* #x7F-#xFFFF (128-65535) */
+        if ($no < 1114112)  return "\\U" . sprintf('%08X', $no);  /* #x10000-#x10FFFF (65536-1114111) */
+        return '';                                                /* not defined => ignore */
     }
 
     /**
@@ -113,7 +183,7 @@ class EasyRdf_Serialiser_Ntriples extends EasyRdf_Serialiser
                 $objects = $resource->all($property);
                 foreach ($objects as $object) {
                     $nt .= $this->ntriplesResource($resource)." ";
-                    $nt .= "<$property> ";
+                    $nt .= "<" . $this->escape($property) . "> ";
                     $nt .= $this->ntriplesObject($object)." .\n";
                 }
             }
