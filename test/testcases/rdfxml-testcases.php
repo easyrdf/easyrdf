8227ebb5106c6e08544d3e17a4bc3ad122d5c46d
Nicholas J Humfrey <njh@aelius.com>
2011-03-08 23:06:11 +0000

    Initial version of PHP script to test the RdfXml parser against the W3C test suite.

---
+++ test/testcases/rdfxml-testcases.php
@@ -0,0 +1,58 @@
+<?php
+    set_include_path(get_include_path() . PATH_SEPARATOR . '../../lib/');
+    require_once "EasyRdf.php";
+
+    # Load the manifest
+    EasyRdf_Namespace::set('test', 'http://www.w3.org/2000/10/rdf-tests/rdfcore/testSchema#');
+    $manifest = parse_testdata("http://www.w3.org/2000/10/rdf-tests/rdfcore/Manifest.rdf");
+
+    $passCount = 0;
+    $failCount = 0;
+
+    foreach($manifest->allOfType('test:PositiveParserTest') as $test) {
+        echo "\n\n";
+
+        echo "Input: ".$test->get('test:inputDocument')."\n";
+        if (!file_exists(testdata_filepath( $test->get('test:inputDocument') ))) {
+            echo "File does not exist.\n";
+            continue;
+        }
+
+        echo "Output: ".$test->get('test:outputDocument')."\n";
+        if (!file_exists(testdata_filepath( $test->get('test:outputDocument') ))) {
+            echo "File does not exist.\n";
+            continue;
+        }
+        
+        $graph = parse_testdata( $test->get('test:inputDocument') );
+        $out_path = testdata_filepath( $test->get('test:outputDocument') );
+
+        $easyrdf_out_path = $out_path . ".easyrdf";
+        file_put_contents($easyrdf_out_path, $graph->serialise('ntriples'));
+        
+        system("rdfdiff -f ntriples -t ntriples $out_path $easyrdf_out_path", $result);
+        if ($result == 0) {
+            echo "OK!\n";
+            $passCount++;
+        } else {
+            echo "Failed!\n";
+            $failCount++;
+        }
+    }
+    
+    echo "Tests that pass: $passCount\n";
+    echo "Tests that fail: $failCount\n";
+
+
+    function testdata_filepath($uri) {
+        return str_replace(
+            "http://www.w3.org/2000/10/rdf-tests/rdfcore/",
+            dirname(__FILE__) . '/rdfxml/', $uri
+        );
+    }
+
+    function parse_testdata($uri) {
+        $filepath = testdata_filepath($uri);
+        $data = file_get_contents($filepath);
+        return new EasyRdf_Graph("$uri", $data, 'rdfxml');
+    }
