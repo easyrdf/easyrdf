11456134876f8fdcb207c0686413cde92e2f998e
Nicholas J Humfrey <njh@aelius.com>
2011-06-23 08:43:58 +0100

    Removed the HTTP namespace,  because it causes problems when automatically adding prefixes to SPARQL queries.

--- lib/EasyRdf/Namespace.php
+++ lib/EasyRdf/Namespace.php
@@ -55,7 +55,6 @@ class EasyRdf_Namespace
       'exif' => 'http://www.w3.org/2003/12/exif/ns#',
       'foaf' => 'http://xmlns.com/foaf/0.1/',
       'geo' => 'http://www.w3.org/2003/01/geo/wgs84_pos#',
-      'http' => 'http://www.w3.org/2006/http#',
       'owl' => 'http://www.w3.org/2002/07/owl#',
       'rdf' => 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
       'rdfs' => 'http://www.w3.org/2000/01/rdf-schema#',
