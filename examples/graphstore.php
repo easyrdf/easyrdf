267a8852d453755fcfec875e78ec2b3e6a72613f
Nicholas J Humfrey <njh@aelius.com>
2011-07-11 08:53:52 +0100

    Cleaned up the graph store example.

--- examples/graphstore.php
+++ examples/graphstore.php
@@ -2,6 +2,13 @@
     /**
      * Store and retrieve data from a SPARQL 1.1 Graph Store
      *
+     * This example adds a triple containing the current time into
+     * a local graph store. It then fetches the whole graph out
+     * and displays the contents.
+     *
+     * Note that you will a graph store, for example RedStore,
+     * running on your local machine in order to test this example.
+     *
      * @package    EasyRdf
      * @copyright  Copyright (c) 2009-2011 Nicholas J Humfrey
      * @license    http://unlicense.org/
@@ -17,24 +24,18 @@
 <body>
 
 <?php
+  // Use a local SPARQL 1.1 Graph Store (eg RedStore)
   $gs = new EasyRdf_GraphStore('http://localhost:8080/data/');
 
-  $graph = new EasyRdf_Graph();
-  #$graph = $gs->get('foobar.rdf');
-  #$gs->delete('foobar.rdf');
-
+  // Add the current time in a graph
+  $graph1 = new EasyRdf_Graph();
+  $graph1->add('http://example.com/test', 'rdfs:label', 'Test');
+  $graph1->add('http://example.com/test', 'dc:date', time());
+  $gs->insert($graph1, 'time.rdf');
   
-  $graph->add('http://time.com/current', 'dc:date', time());
-
-  echo $graph->dump();
-
-  $gs->insert($graph, 'time.rdf');
-  
-  
-//   $gs->insert($graph);
-//   $gs->insert($graph, 'new.rdf');
-  
-  
+  // Get the graph back out of the graph store and display it
+  $graph2 = $gs->get('time.rdf');
+  print $graph2->dump();
 ?>
 
 </body>
