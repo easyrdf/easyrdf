4793e89a618d0e6ce5a594cebcc4ec3df8406a85
Nicholas J Humfrey <njh@aelius.com>
2012-12-08 16:11:36 +0000

    Code style

--- test/EasyRdf/Parser/RedlandTest.php
+++ test/EasyRdf/Parser/RedlandTest.php
@@ -86,7 +86,7 @@ class EasyRdf_Parser_RedlandTest extends EasyRdf_TestCase
         $count = $this->_parser->parse(
             $this->_graph,
             "<rdf:RDF xmlns:rdf='http://www.w3.org/1999/02/22-rdf-syntax-ns#' ".
-	          "         xmlns:foaf='http://xmlns.com/foaf/0.1/' >".
+            "         xmlns:foaf='http://xmlns.com/foaf/0.1/' >".
             "  <rdf:Description rdf:nodeID='bob'>".
             "    <foaf:name>Bob</foaf:name>".
             "  </rdf:Description>".
