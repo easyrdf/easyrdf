72c58dac5d22fa864b638f964ae39236a2f74656
Nicholas Humfrey <njh@aelius.com>
2020-06-03 23:22:28 +0100

    Added trailing slash to RDF/XML URI (the match all the other W3C format URIs)

--- lib/Format.php
+++ lib/Format.php
@@ -614,7 +614,7 @@ Format::register(
 Format::register(
     'rdfxml',
     'RDF/XML',
-    'http://www.w3.org/TR/rdf-syntax-grammar',
+    'http://www.w3.org/TR/rdf-syntax-grammar/',
     array(
         'application/rdf+xml' => 0.8
     ),
